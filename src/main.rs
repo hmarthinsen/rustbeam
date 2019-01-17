0aedb3adf78671401cfb3716e488017599bf31c1
Håkon Marthinsen <hakon.marthinsen@gmail.com>
2019-01-17 22:02:09 +0100

    Show a black window with SDL

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("rust-sdl2 demo", 1280, 720)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'render_loop: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'render_loop,
+                _ => {}
+            }
+        }
+        // TODO: Drawing goes here.
+
+        canvas.present();
+    }
+}
